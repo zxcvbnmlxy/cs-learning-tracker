6ec90ed64bffcd28e71ef01c50e61b6944276172
lxy <2701235591@qq.com>
2026-07-27 23:29:09 +0800

    2026-7-27

---
+++ 01-cpp-learning/modern_cpp/gdb_exercise.cpp
@@ -0,0 +1,54 @@
+// GDB 深入练习 — 找出程序中所有的 bug
+// 功能：输入一个数组，计算最大值、最小值、平均值，并反转数组
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n){
+    for(int i=0;i<=n;i++){      // ？？
+        int t=arr[i];
+        arr[i]=arr[n-i];
+        arr[n-i]=t;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"输入数组大小: ";
+    cin>>n;
+    int arr[5];                 // ？？
+    cout<<"输入"<<n<<"个数:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    // 计算最大值
+    int maxVal;
+    for(int i=0;i<n;i++){       // ？？
+        if(arr[i]>maxVal)
+            maxVal=arr[i];
+    }
+
+    // 计算最小值
+    int minVal=maxVal;
+    for(int i=0;i<n;i++){
+        if(arr[i]<minVal)
+            minVal=arr[i];
+    }
+
+    // 计算平均值
+    int sum;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    double avg=(double)sum/n;
+
+    cout<<"max="<<maxVal<<" min="<<minVal<<" avg="<<avg<<"\n";
+
+    reverseArray(arr,n);
+
+    cout<<"反转后: ";
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    cout<<"\n";
+    return 0;
+}
